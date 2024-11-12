45179c5f80fed2cb4b7ee4bb063f61dc6ac77a6e
radixdt2916 <radixdt.2916@gmail.com>
2024-11-12 15:41:34 +0530

    added reboot script

--- reboot.ps1
+++ reboot.ps1
@@ -1,11 +1,32 @@
-Write-Host "Attempting to reboot the system..." -ForegroundColor Yellow
+$LogFilePath = "D:\Reboot_logs_through_pipeline\RebootLog.txt"
+
+function Get-Timestamp {
+    return (Get-Date -Format "yyyy-MM-dd HH:mm:ss")
+}
+
+function Log-Message {
+    param (
+        [string]$Message,
+        [string]$Color
+    )
+    $TimestampedMessage = "$(Get-Timestamp): $Message"
+    Write-Host $TimestampedMessage -ForegroundColor $Color
+    $TimestampedMessage | Out-File -FilePath $LogFilePath -Append
+}
+
+Log-Message "Starting reboot process..." "Yellow"
 
 try {
+    Log-Message "Attempting to reboot the system..." "Yellow"
+
     Restart-Computer -Force -ErrorAction Stop
-    Write-Host "Reboot command initiated successfully." -ForegroundColor Green
+    Log-Message "Reboot command initiated successfully." "Green"
 } catch {
-    Write-Host "Failed to reboot the system. Error: $_" -ForegroundColor Red
+    Log-Message "Failed to reboot the system. Error: $_" "Red"
     exit 1
 }
+
+Log-Message "Process completed. System reboot command was issued." "Green"
+Log-Message "========================================================" "White"
 
 exit 0
